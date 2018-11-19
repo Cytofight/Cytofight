265a714be13bd489a59b0f0cbe674a3884e435e3
Evangelos Vekios <evekios@gmail.com>
2018-11-19 15:56:45 -0500

    adds click-to-play button

--- client/components/GameFunctions/create.js
+++ client/components/GameFunctions/create.js
@@ -1,6 +1,12 @@
-import { players, keyboardControls, scoreAndStars, NPCCells } from './createFunctions'
+import {
+  players,
+  keyboardControls,
+  scoreAndStars,
+  NPCCells
+} from './createFunctions'
 
 export function preload() {
+  this.load.image('click', 'assets/PNG/play.png')
   this.load.image('ship', 'assets/PNG/b_cell.png')
   this.load.image('otherPlayer', 'assets/PNG/whitebloodcell.png')
   this.load.image('star', 'assets/PNG/star_gold.png')
@@ -12,20 +18,28 @@ export function preload() {
 }
 
 export function create() {
-  // PUT IN A SETUP FUNC
-  this.matter.world.setBounds(0, 0, 1000, 1000)
-  this.cameras.main.setBounds(0, 0, 1000, 1000)
-  players.call(this)
-  keyboardControls.call(this)
-  // scoreAndStars.call(this)
-  NPCCells.call(this)
-  // this.matter.world.on('collisionstart', (event, bodyA, bodyB) => {
-  //   console.log('collision detected, emitting bodies:', bodyA)
-  //   console.log('ship id: ', this.ship.body.id)
-  //   if (bodyA.id === this.ship.body.id) console.log('THEY MATCH')
-  //   // this.socket.emit('anyCollision', bodyA, bodyB)
-  // })
-  // this.socket.on('collided', (bodyA, bodyB) => {
-  //   console.log('WHOLE DATAS: ', bodyA, bodyB)
-  // })
+  const play = this.add.image(400, 300, 'click').setInteractive()
+  play.once(
+    'pointerup',
+    function() {
+      play.setVisible(false)
+      // PUT IN A SETUP FUNC
+      this.matter.world.setBounds(0, 0, 1000, 1000)
+      this.cameras.main.setBounds(0, 0, 1000, 1000)
+      players.call(this)
+      keyboardControls.call(this)
+      // scoreAndStars.call(this)
+      NPCCells.call(this)
+      // this.matter.world.on('collisionstart', (event, bodyA, bodyB) => {
+      //   console.log('collision detected, emitting bodies:', bodyA)
+      //   console.log('ship id: ', this.ship.body.id)
+      //   if (bodyA.id === this.ship.body.id) console.log('THEY MATCH')
+      //   // this.socket.emit('anyCollision', bodyA, bodyB)
+      // })
+      // this.socket.on('collided', (bodyA, bodyB) => {
+      //   console.log('WHOLE DATAS: ', bodyA, bodyB)
+      // })
+    },
+    this
+  )
 }
