5d7d4b4038b5e21411a8a87978755ec20653fa77
Eric Roginek <eroginek@gmail.com>
2018-11-20 13:57:42 -0500

    Socket crash bug fixed, epthelial cells now change color to red if touched by a red player

--- client/components/GameFunctions/create.js
+++ client/components/GameFunctions/create.js
@@ -17,14 +17,18 @@ export function create() {
   this.cameras.main.setBounds(0, 0, 1000, 1000)
   players.call(this)
   keyboardControls.call(this)
-  scoreAndStars.call(this)
+  // scoreAndStars.call(this)
   NPCCells.call(this)
-  // this.matter.world.on('collisionstart', (event, bodyA, bodyB) => {
-  //   console.log('collision detected, emitting bodies:', bodyA)
-  //   console.log('ship id: ', this.ship.body.id)
-  //   if (bodyA.id === this.ship.body.id) console.log('THEY MATCH')
-  //   // this.socket.emit('anyCollision', bodyA, bodyB)
-  // })
+  this.matter.world.on('collisionstart', (event, bodyA, bodyB) => {
+    // console.log('collision detected, emitting bodies:', bodyA)
+    // console.log('ship id: ', this.ship.body.id)
+    // console.log(this.epithelialCells)
+    const matchingCell = this.epithelialCells.find(cell => (cell.body.id === bodyA.id || cell.body.id === bodyB.id))
+    if (this.ship && matchingCell && (bodyA.id === this.ship.body.id || bodyB.id === this.ship.body.id) && (this.ship.tintBottomLeft === 214)) {
+      matchingCell.setTint(0xd60000)
+    }
+    // this.socket.emit('anyCollision', bodyA, bodyB)
+  })
   // this.socket.on('collided', (bodyA, bodyB) => {
   //   console.log('WHOLE DATAS: ', bodyA, bodyB)
   // })
