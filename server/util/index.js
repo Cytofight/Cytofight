225af8d60f243cc31de808f2fcd4484c2a2c6b32
ARKielley <eatingfoil@gmail.com>
2018-11-24 21:51:59 -0500

    deleted unnecessary directory

--- server/util/index.js
+++
@@ -1,9 +0,0 @@
-function fire () {
-  console.log("FIRE!!! But working now! I swear!")
-  let antibody = this.antibodies.get();
-  if(antibody) {
-    antibody.fire(this.ship.body.position.x, this.ship.body.position.y, this.ship.body.angle);
-  }
-}
-
-module.exports = {fire}
