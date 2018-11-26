08065e6e3097a7fb708d3dfcb6c07943c4cb9c20
Jeb Roberts <jebroberts2@gmail.com>
2018-11-26 11:23:53 -0500

    clear antibodies

---
+++ Animations/Brain_Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Brain_Test" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="../public/assets/PNG/brain-background.png" width="284" height="177"/>
+</tileset>
