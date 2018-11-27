b3fb23f0d774a00b98719a1554e6112e1910c0a1
Eric Roginek <eroginek@gmail.com>
2018-11-27 12:28:20 -0500

    Background image resized

--- client/components/GameFunctions/create.js
+++ client/components/GameFunctions/create.js
@@ -16,20 +16,18 @@ export function preload() {
   this.load.image('antibody', 'assets/PNG/antibody-game-transparent.png')
   this.load.image('dormantTCell', 'assets/PNG/White_blood_cell_transparent.png')
   this.load.image('epithelialCell', 'assets/PNG/epithelial_transparent.png')
+
+  // Background image: make sure file is compressed using https://imagecompressor.com/
   this.load.image('redback', 'assets/PNG/redback.png')
 }
 
 export function create() {
-//*************************************************************** */
-  // This code is for rendering an image to the canvas background
-  // Problem: any image large enough to cover the world canvas size is too large to be pushed to github;
-  // Phaser 3.0 seems to only be able to load images from a local assets folder and is restricted to using PNG images
-  this.add.image((worldSize.x/2), (worldSize.y/2), 'redback')
-//*************************************************************** */
-
   //  The world is 3200 x 600 in size
   this.cameras.main.setBounds(0, 0, worldSize.x, worldSize.y).setName('main')
-
+  
+  // Create canvas background image
+  this.add.image((worldSize.x/2), (worldSize.y/2), 'redback').setScale(1.9)
+  
   //  The miniCam is 400px wide, so can display the whole world at a zoom of 0.2
   this.minimap = this.cameras
     .add(640, 490, 150, 100)
