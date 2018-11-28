d7d87bd89418d29a186484f4c569e84f47e102e7
Evangelos Vekios <evekios@gmail.com>
2018-11-28 16:28:38 -0500

    cleans up game page

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,32 +3,72 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {withStyles, CssBaseline, Toolbar} from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const styles = theme => ({
+  text: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2
+  },
+  paper: {
+    paddingBottom: 50
+  },
+  list: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  subHeader: {
+    backgroundColor: theme.palette.background.paper
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0
+  },
+  toolbar: {
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  fabButton: {
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto'
+  }
+})
+
+const Navbar = ({handleClick, isLoggedIn, classes}) => (
   <div>
-    <h1>CYTOFIGHT</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/game">Play!</Link>
-          <Link to="/heroes">Meet the Heroes</Link>
-          <Link to="/villains">Meet the Villains</Link>
-          <Link to="/messages">Messages</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
+    <CssBaseline />
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/game">Play!</Link>
+            <Link to="/heroes">Meet the Heroes</Link>
+            <Link to="/villains">Meet the Villains</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/game">Play!</Link>
+            <Link to="/heroes">Meet the Heroes</Link>
+            <Link to="/villains">Meet the Villains</Link>
+            <Link to="/messages">Messages</Link>
+          </div>
+        )}
+      </Toolbar>
+      <hr />
+    </AppBar>
   </div>
 )
 
@@ -49,12 +89,13 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(withStyles(styles)(Navbar))
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
