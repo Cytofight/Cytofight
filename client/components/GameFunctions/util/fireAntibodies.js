8dac7c685662aa5fca504411885319f9f9a266e9
Eric Roginek <eroginek@gmail.com>
2018-11-29 08:47:51 -0500

    Firing antibodies now produce a fire sound

--- client/components/GameFunctions/util/fireAntibodies.js
+++ client/components/GameFunctions/util/fireAntibodies.js
@@ -1,28 +1,32 @@
 // Fire antibodies function for memory B cells (main character for good guys)
-export function fire (prevInfo) {
-    let firingInfo
-    if (!prevInfo) {
-      let randomDamage = Math.floor(Math.random() * 10) + 10
-      let randomColor = Math.floor(Math.random() * 16777215)
-      if (this.secretColor.found) {
-        randomColor = this.secretColor.value
-      }
-      firingInfo = {
-        x: this.ship.body.position.x,
-        y: this.ship.body.position.y,
-        angle: this.ship.body.angle,
-        globalId: this.socket.id,
-        type: 'ship',
-        color: randomColor,
-        damage: randomDamage
-      }
-    } else {
-      firingInfo = prevInfo
+export function fire(prevInfo) {
+  const fireSound = this.sound.add('shoot', {
+    volume: 0.5
+  })
+  let firingInfo
+  if (!prevInfo) {
+    let randomDamage = Math.floor(Math.random() * 10) + 10
+    let randomColor = Math.floor(Math.random() * 16777215)
+    if (this.secretColor.found) {
+      randomColor = this.secretColor.value
     }
-    let antibody = this.antibodies.get();
-    if(antibody) {
-      antibody.setTint(firingInfo.color)
-      antibody.fire(firingInfo);
-      if (!prevInfo) this.socket.emit('firedAntibody', firingInfo)
+    firingInfo = {
+      x: this.ship.body.position.x,
+      y: this.ship.body.position.y,
+      angle: this.ship.body.angle,
+      globalId: this.socket.id,
+      type: 'ship',
+      color: randomColor,
+      damage: randomDamage
     }
+  } else {
+    firingInfo = prevInfo
   }
+  let antibody = this.antibodies.get();
+  if (antibody) {
+    antibody.setTint(firingInfo.color)
+    antibody.fire(firingInfo)
+    fireSound.play()
+    if (!prevInfo) this.socket.emit('firedAntibody', firingInfo)
+  }
+}
