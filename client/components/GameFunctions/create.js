3882c1ee66bf03368547b1a9dbbe55d13ff4848b
Evangelos Vekios <evekios@gmail.com>
2018-11-29 15:20:04 -0500

    fixes score and minimap issues

--- client/components/GameFunctions/create.js
+++ client/components/GameFunctions/create.js
@@ -27,12 +27,13 @@ export function create() {
 
   //  The miniCam is 400px wide, so can display the whole world at a zoom of 0.2
   this.minimap = this.cameras
-    .add(window.innerWidth - 165, window.innerHeight - 100, 150, 100)
+    .add(0, window.innerHeight - 100, 150, 100)
     .setZoom(0.1)
     .setName('mini')
+  // this.minimap.blueScoreText.setText('')
   this.minimap.setBackgroundColor(0x002244)
-  this.minimap.scrollX = 2000
-  this.minimap.scrollY = 2000
+  this.minimap.scrollX = worldSize.x
+  this.minimap.scrollY = worldSize.y
 
   // PUT IN A SETUP FUNC
   this.matter.world.setBounds(0, 0, worldSize.x, worldSize.y)
@@ -47,21 +48,24 @@ export function create() {
 
   console.log(this.cameras)
   this.blueScoreText = this.add
-    .text(16, 16, '', {fontSize: '16px', fill: '#01c0ff'})
+    .text(window.innerWidth - 440, 16, '', {
+      fontSize: '24px',
+      fontStyle: 'bold',
+      fill: 'blue'
+    })
     .setDepth(1)
     .setScrollFactor(0)
-    .setShadow(3, 3, 'black', 3, true, true)
-    .setStroke('yellow', 4)
+    .setStroke('yellow', 2)
 
   this.redScoreText = this.add
-    .text(window.innerWidth - 300, 16, '', {
-      fontSize: '16px',
+    .text(window.innerWidth - 440, 48, '', {
+      fontSize: '24px',
+      fontStyle: 'bold',
       fill: '#d60000'
     })
     .setDepth(1)
     .setScrollFactor(0)
-    .setShadow(3, 3, 'black', 3, true, true)
-    .setStroke('yellow', 4)
+    .setStroke('yellow', 2)
 
   scoreAndStars.call(this)
 }
