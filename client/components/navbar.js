5bb0b4f78c16b06a1c504ce8b70e33f7baba5afb
Evangelos Vekios <evekios@gmail.com>
2018-11-30 09:42:26 -0500

    tiny navbar changes

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -60,7 +60,7 @@ class Navbar extends React.Component {
 
     return (
       <div position="fixed">
-        <Button onClick={this.toggleDrawer('left', true)}>MENU</Button>
+        <Button onClick={this.toggleDrawer('left', true)}>...</Button>
         <Drawer
           open={this.state.left}
           onClose={this.toggleDrawer('left', false)}
